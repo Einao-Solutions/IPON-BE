870f54665408b8daf7713527c0d22e57d39c6982
Joel <joel,eriba@einaosolutions.com>
2026-02-26 08:36:25 +0100

    Add userId to TreatRecordalDto; add PerformanceDto class
    Added userId property to TreatRecordalDto for user tracking. Introduced PerformanceDto in StatisticsDto.cs to capture application performance and status change details.

---
+++ patentdesign/Dtos/Request/StatisticsDto.cs
@@ -0,0 +1,18 @@
+﻿using patentdesign.Enums;
+using patentdesign.Models;
+
+namespace patentdesign.Dtos.Request
+{
+    public class PerformanceDto
+    {
+        public FormApplicationTypes? ApplicationType { get; set; }
+        public ApplicationStatuses? BeforeStatus { get; set; }
+        public ApplicationStatuses? AfterStatus { get; set; }
+        public string? Reason { get; set; }
+        public DateTime? Date { get; set; }
+        public string? AppUserId { get; set; }
+        public Roles? OfficeUnit { get; set; }
+        public string? FileNumber { get; set; }
+        public FileTypes? FileType { get; set; }
+    }
+}
