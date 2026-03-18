7d06f7fa535c3d60f3a5b9e2d463f9ee5d0972a0
Joel <joel,eriba@einaosolutions.com>
2026-03-18 13:01:52 +0100

    Update CORS origins and deployment destination path
    Adjusted CORS policy origins in Program.cs, removing "https://iponigeria.einaotest.com" and ensuring "https://link.einaotest.com" is included. Changed deployment path in azure-pipelines-prod.yml to "C:\inetpub\vhosts\iponigeria.com\integration.iponigeria.com".

--- patentdesign/Program.cs
+++ patentdesign/Program.cs
@@ -82,8 +82,7 @@ builder.Services.AddCors(options =>
             .WithOrigins(
                 "https://portal.iponigeria.com",
                 "http://localhost:5173",
-                "https://link.einaotest.com",
-                "https://iponigeria.einaotest.com"
+                "https://link.einaotest.com"
             )
             .AllowAnyHeader()
             .AllowAnyMethod()
